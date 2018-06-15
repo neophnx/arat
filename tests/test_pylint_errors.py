09d2edd8e8d3e5d8185e17a4d4e45138d849eb06
Julien Gosme <julien.gosme@yahoo.fr>
2018-06-15 22:37:41 +0200

    issue #7: tests/test_pylint_errors.py score 10.00/10

--- tests/test_pylint_errors.py
+++ tests/test_pylint_errors.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
 Created on Fri Jun  8 21:09:59 2018
@@ -6,29 +5,60 @@ Created on Fri Jun  8 21:09:59 2018
 @author: phnx
 """
 
-from unittest import TestCase
+# future
+from __future__ import print_function
+
+# standard
+import unittest
+
+# third party
 from pylint.lint import Run
 from pylint.reporters import BaseReporter
-from pprint import pformat
 
 
 class DummyReporter(BaseReporter):
+    """
+    A pylint reporter ignoring all messages
+    Typical usage: running pylint to get overall statistics (like score)
+    """
+
     def __init__(self, output=None):
         BaseReporter.__init__(self, output)
 
     def add_message(self, msg_id, location, msg):
+        """
+        ignore all message coming from pylint
+        """
         pass
 
     def _display(self, layout):
+        """
+        don't display anything
+        """
         pass
 
 
-class PylintTestCase(TestCase):
+class PylintTestCase(unittest.TestCase):
+    """
+    All errors found staticaly by pylint fail this unit test
+    """
 
-    def test_score(self):
+    def test_pylint_errors(self):
+        """
+        check if pylint reports errors
+        """
         out = DummyReporter()
-        r = Run(['--errors-only', "--jobs=8", 'standalone',
-                 'server', 'tests'], reporter=out, exit=False)
-#        self.assertEquals(len(out.messages), 2, msg=str(out.messages))
-        self.assertEquals(
-            r.linter.stats["error"], 0, "%i pylint error found: please run make static-test for further detail" % r.linter.stats["error"])
+        run = Run(['--errors-only', "--jobs=8", 'standalone',
+                   'server', 'tests'], reporter=out, exit=False)
+        self.assertEquals(run.linter.stats["error"],
+                          0,
+                          "%i pylint error found: "
+                          "please run `make static-test{2,3}' "
+                          "for detail report" % run.linter.stats["error"])
+
+
+if __name__ == "__main__":
+    import sys
+    print("Please wait, this unit test is quite long...")
+    SUITE = unittest.TestLoader().loadTestsFromTestCase(PylintTestCase)
+    unittest.TextTestRunner(verbosity=3, stream=sys.stdout).run(SUITE)
